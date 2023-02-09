0a674ad7e1495a72feb6b3bd91e9313d693d2904
Alex Poirier <alexmatthewpoirier@gmail.com>
2023-02-09 16:25:20 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+let sizeOfGrid = 16;
+const resetButton = document.querySelector("button");
+const container = document.querySelector(".container");
+
+const createGrid = (amtOfGrids) => {
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("wrapper");
+
+    for (let i = 0; i < amtOfGrids; i++) {
+        const row = document.createElement("div");
+        row.classList.add("grid-row");
+
+        for (let j = 0; j < amtOfGrids; j++) {
+            const widthAndHeight = 960 / amtOfGrids;
+            const gridBox = document.createElement("div");
+            gridBox.classList.add("grid-box");
+            gridBox.style.width = `${widthAndHeight}px`;
+            gridBox.style.height = `${widthAndHeight}px`;
+
+            gridBox.addEventListener("mouseenter", () => {
+                gridBox.style.backgroundColor = "black";
+            });
+            row.appendChild(gridBox);
+        };
+
+        wrapper.appendChild(row);
+    };
+    container.appendChild(wrapper);
+};
+
+resetButton.addEventListener("click" , () => {
+    let userSize = Number(prompt("what dimensions do you want for the new grid?"));
+
+    while(userSize > 100) {
+        userSize = Number(prompt("Pick a smaller number and make sure it's 100 or less!"));
+
+    };
+    const wrapper = document.querySelector(".wrapper");
+    
+    if (!wrapper) {
+        createGrid(userSize)
+    } else {
+        wrapper.remove();
+        createGrid(userSize);
+    }
+});
+
